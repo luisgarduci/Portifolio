bcca23abcdd0629a06ff97ec5b9df6915660ddc9
luisgarduci <112954236+luisgarduci@users.noreply.github.com>
2024-02-28 12:49:13 -0300

    alterações

--- scripts/script.js
+++ scripts/script.js
@@ -47,11 +47,11 @@ let image = document.getElementById("image-project");
 let name = document.getElementById("name-project");
 let description = document.getElementById("description-project");
 let link = document.getElementById("link-project");
-let arrayProjectImage = ["spectrus.png", "tempo.png", "tarefas.jpeg", "pokemon.jpg", "https://imovelguide.com.br/images/logo-imovel-guide.png"];
-let arrayProjectName = ["Spectrus", "Previsão do Tempo", "Lista de Tarefas", "Pokedex", "ImovelGuide"];
-let arraydescriptionProject = ["(em andamento) Este projeto é um site/aplicativo de alguel para academias, está sendo desenvolvido com HTML, CSS, Javascript, PHP, Ajax e SQL. Os usuários poderão ver seus treinos, acompanhar sua evolução, procurar pelo perfil de outros usuários, etc...", "É uma aplicação web que fornece informações sobre a previsão do tempo para qualquer localização. A aplicação utiliza a integração com uma API de previsão do tempo para fornecer dados atualizados, incluindo temperatura, condições climáticas, umidade e velocidade do vento.", "Este projeto foi  desenvolvido usando HTML, CSS e JavaScript. Ele oferece uma solução eficaz para o gerenciamento de tarefas diárias. A interface do usuário é projetada de forma responsiva, garantindo uma experiência consistente em computadores, tablets e dispositivos móveis.", "A Pokédex é um projeto de website que permite aos usuários procurarem um pokemon específico. Este projeto foi desenvolvido com HTML, CSS e JavaScript e consome a API da Pokédex, uma fonte de dados com informações sobre Pokémons.", "Teste técnico da empresa, visando o domínio de HTML, CSS E Javascript, projeto 100% responsivo e adaptável, possui utilização do plugin do Jquery Mask para máscaras de cpf e telefone, e a biblioteca do HTMLCanvas2 para transformar código em imagem"];
+let arrayProjectImage = ["spectrus.png", "tempo.png", "tarefas.jpeg", "pokemon.jpg", "https://imovelguide.com.br/images/logo-imovel-guide.png", "filme.jpg"];
+let arrayProjectName = ["Spectrus", "Previsão do Tempo", "Lista de Tarefas", "Pokedex", "ImovelGuide", "Movies"];
+let arraydescriptionProject = ["(em andamento) Este projeto é um site/aplicativo de alguel para academias, está sendo desenvolvido com HTML, CSS, Javascript, PHP, Ajax e SQL. Os usuários poderão ver seus treinos, acompanhar sua evolução, procurar pelo perfil de outros usuários, etc...", "É uma aplicação web que fornece informações sobre a previsão do tempo para qualquer localização. A aplicação utiliza a integração com uma API de previsão do tempo para fornecer dados atualizados, incluindo temperatura, condições climáticas, umidade e velocidade do vento.", "Este projeto foi  desenvolvido usando HTML, CSS e JavaScript. Ele oferece uma solução eficaz para o gerenciamento de tarefas diárias. A interface do usuário é projetada de forma responsiva, garantindo uma experiência consistente em computadores, tablets e dispositivos móveis.", "A Pokédex é um projeto de website que permite aos usuários procurarem um pokemon específico. Este projeto foi desenvolvido com HTML, CSS e JavaScript e consome a API da Pokédex, uma fonte de dados com informações sobre Pokémons.", "Teste técnico da empresa, visando o domínio de HTML, CSS E Javascript, projeto 100% responsivo e adaptável, possui utilização do plugin do Jquery Mask para máscaras de cpf e telefone, e a biblioteca do HTMLCanvas2 para transformar código em imagem", "Esse projeto permite aos usuários explorar e descobrir uma ampla variedade de filmes utilizando a API do TMDb (The Movie Database). Com uma interface simples e intuitiva, os usuários podem acessar uma vasta coleção de informações sobre filmes, desde os mais populares até os mais obscuros."];
 
-let arrayLinkProject = ["http://spcetrus.free.nf", "https://luisgarduci.github.io/Tempo/", "https://luisgarduci.github.io/Tarefas/","https://luisgarduci.github.io/Pokedex/", "https://luisgarduci.github.io/ImovelGuide/"];
+let arrayLinkProject = ["http://spcetrus.free.nf", "https://luisgarduci.github.io/Tempo/", "https://luisgarduci.github.io/Tarefas/","https://luisgarduci.github.io/Pokedex/", "https://luisgarduci.github.io/ImovelGuide/", "http://filmes.wuaze.com/Filme/index.php"];
 
 
 image.src = arrayProjectImage[index];
@@ -61,7 +61,7 @@ link.href = arrayLinkProject[index];
 
 right_arrows.forEach (right_arrow => {
     right_arrow.addEventListener("click", () => {
-        if (index < 4) {
+        if (index < 5) {
             index++;
             image.src = arrayProjectImage[index];
             name.textContent = arrayProjectName[index];
